2f9874dab912807a279b2681b4c492d0b3898798
alejandroforcada <alejandroforcadacs@gmail.com>
2023-09-27 08:53:32 +0200

    headers acabado

---
+++ headers/download.php
@@ -0,0 +1,16 @@
+<?php 
+header('Content-Type: application/zip');
+
+header('Content-Length: 100000');
+
+header('Content-Disposition: attachment; filename="download.zip"');
+
+for ($i=0;$i<1000;$i++){
+
+    echo str_repeat(".",1000);
+
+
+    usleep(50000);
+}
+
+?>
