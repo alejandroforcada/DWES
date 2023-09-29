abec3c6959c662c21421008a3415267b47a22e92
alejandroforcada <aluper2170@ieselcaminas.org>
2023-09-29 21:49:38 +0100

    crud finalizado

---
+++ CRUD/select.php
@@ -0,0 +1,28 @@
+<?php
+require('Producto.php');
+
+require('conexion.php');
+$precio = $_GET['id'] ?? 1;
+
+$pdoSt= $pdo->query("SELECT nombre,codigodebarras,precio from producto WHERE id = ?");
+$pdoSt->bindParam(1, $id);
+$pdoSt->execute();
+
+$producto = $pdoSt->fetch();
+
+echo "Nombre: " . $producto['nombre'] . "<br>";
+echo "Codigo de barras: " . $producto['codigodebarras'] . "<br>";
+echo "Precio: " . $producto['precio'] . "<br>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
